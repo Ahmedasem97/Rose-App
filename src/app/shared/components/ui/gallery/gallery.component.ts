7e30f7d15b53a5b0fd8f005a432a496667856dc4
Ahmed Ashraf <a.ashraf.elbendary@gmail.com>
2025-01-30 20:42:36 +0300

    Add section tag & section title componenets

--- src/app/shared/components/ui/gallery/gallery.component.ts
+++ src/app/shared/components/ui/gallery/gallery.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { galleryMockData } from '../../../../mock/gallery.mock';
+import { SectionTagComponent } from '../section-tag/section-tag.component';
+import { SectionTitleComponent } from '../section-title/section-title.component';
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [],
+  imports: [SectionTagComponent, SectionTitleComponent],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.scss',
 })
