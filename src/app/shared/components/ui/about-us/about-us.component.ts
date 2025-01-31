0111c8e1eb53e769e4ca082dac8d8a051021bd1c
Ahmed Ashraf <a.ashraf.elbendary@gmail.com>
2025-01-31 20:47:40 +0300

    Complete Right Side

--- src/app/shared/components/ui/about-us/about-us.component.ts
+++ src/app/shared/components/ui/about-us/about-us.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SectionTagComponent } from '../section-tag/section-tag.component';
 import { PrimaryBtnComponent } from '../primary-btn/primary-btn.component';
+import { appFeaturesMockData } from '../../../../mock/app-features.mock';
 @Component({
   selector: 'app-about-us',
   standalone: true,
@@ -8,4 +9,6 @@ import { PrimaryBtnComponent } from '../primary-btn/primary-btn.component';
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss',
 })
-export class AboutUsComponent {}
+export class AboutUsComponent {
+  featuresList: string[] = appFeaturesMockData;
+}
