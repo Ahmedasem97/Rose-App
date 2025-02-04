9a66989297a3b0877f37c11d8dd47d0921b9c92b
Ahmed Assem <ahmedasem224@gmail.com>
2025-02-04 19:59:16 +0200

    create modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./core/layout/navbar/navbar.component";
-import { NavbarAuthComponent } from "./core/layout/navbar-auth/navbar-auth.component";
 import { FooterComponent } from "./core/layout/footer/footer.component";
 import { FlowbiteService } from './core/services/flowbite.service';
+import { AuthModalComponent } from "./shared/components/ui/auth-modal/auth-modal.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, AuthModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
