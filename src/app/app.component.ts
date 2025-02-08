ac93359b095d4a477b79582ed4f584e4da58d520
Ahmed Assem <ahmedasem224@gmail.com>
2025-02-08 17:01:49 +0200

    create forgetPassword ui and use usable input and btn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./core/layout/navbar/navbar.component";
 import { FooterComponent } from "./core/layout/footer/footer.component";
 import { FlowbiteService } from './core/services/flowbite.service';
+import { ModalComponent } from "./shared/components/ui/modal/modal.component";
+import { ForgetPasswordComponent } from "./shared/components/ui/forget-password/forget-password.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, ModalComponent, ForgetPasswordComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/shared/components/ui/forget-password/forget-password.component.ts
+++ src/app/shared/components/ui/forget-password/forget-password.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { PrimaryBtnComponent } from "../primary-btn/primary-btn.component";
+import { CustomInputComponent } from "../../business/custom-input/custom-input.component";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forget-password',
   standalone: true,
-  imports: [PrimaryBtnComponent],
+  imports: [PrimaryBtnComponent, CustomInputComponent, ReactiveFormsModule],
   templateUrl: './forget-password.component.html',
   styleUrl: './forget-password.component.scss'
 })
 export class ForgetPasswordComponent {
+
+  forgetPassword: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+  })
+
 
 }
