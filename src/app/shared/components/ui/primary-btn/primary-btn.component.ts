2d671466f74b8e27799aec2b0433bae67f6f67d3
Ahmed Ashraf <a.ashraf.elbendary@gmail.com>
2025-02-08 19:28:15 +0300

    Remove unused imports

--- src/app/shared/components/ui/primary-btn/primary-btn.component.ts
+++ src/app/shared/components/ui/primary-btn/primary-btn.component.ts
@@ -1,11 +1,4 @@
-import {
-  Component,
-  EventEmitter,
-  input,
-  Input,
-  output,
-  Output,
-} from '@angular/core';
+import { Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-primary-btn',
