a9b696dca4b90a1d95f2d31ffb9006c7ae8db4cc
Ahmed Ashraf <a.ashraf.elbendary@gmail.com>
2025-02-08 19:15:02 +0300

    Start the login logic

--- src/app/core/pages/login/login.component.ts
+++ src/app/core/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CustomInputComponent } from '../../../shared/components/business/custom-input/custom-input.component';
 
 import {
@@ -7,27 +7,64 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { PrimaryBtnComponent } from '../../../shared/components/ui/primary-btn/primary-btn.component';
+import { TokenManagerService } from '../../../shared/services/token-manager.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CustomInputComponent, ReactiveFormsModule],
+  imports: [CustomInputComponent, ReactiveFormsModule, PrimaryBtnComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-  loginForm = new FormGroup({
-    email: new FormControl('', [Validators.email, Validators.required]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.pattern(
-        `^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$`
-      ),
-    ]),
-  });
+export class LoginComponent implements OnInit {
+  isSubmitted: boolean = false;
+  canNavigate: boolean = false;
+  loginForm!: FormGroup;
+
+  private readonly _tokenManagerService = inject(TokenManagerService);
+
+  initLoginForm() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern(
+          `^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$`
+        ),
+      ]),
+    });
+  }
+
+  ngOnInit(): void {
+    this.initLoginForm();
+  }
 
   submit() {
-    console.log('s');
+    this.isSubmitted = true;
+    // this._AuthApiManagerService.login(baseUrl, data).subscribe({
+    //   next: (res) => {
+    //     this.isSubmitted = false;
+    //     let severity = '';
+    //     let title = '';
+    //     let message = '';
+    //     if (res.message == 'success') {
+    //       severity = 'success';
+    //       title = 'Welcome!';
+    //       message = 'You have logged-in successfully';
+    //       // this._TokenManagerService.setToken(res.token);
+    //       this.canNavigate = true;
+    //     } else {
+    //       let errorMsg = res.error.message;
+    //       severity = 'error';
+    //       title = 'Error!';
+    //       message = errorMsg;
+    //     }
+
+    //     // this._Toaster.showToaster(severity, title, message);
+    //     // this.canNavigate && this._Router.navigate(['/main/dashboard']);
+    //   },
+    // });
     console.log(this.loginForm);
   }
 }
