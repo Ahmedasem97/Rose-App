c687c48da710ac4a1b67d1987f627a1b40448d6f
Ahmed Assem <ahmedasem224@gmail.com>
2025-02-09 19:44:34 +0200

    create folder environment to add baseUrl

--- projects/auth-lib/src/lib/enums/Auth.endpoint.ts
+++ projects/auth-lib/src/lib/enums/Auth.endpoint.ts
@@ -1,14 +1,14 @@
 
 
 export class ApiEndpoint {
-    static LOGIN = "https://exam.elevateegy.com/api/v1/auth/signin"
-    static REGISTER = "https://exam.elevateegy.com/api/v1/auth/signup"
-    static CHANGE_PASSWORD = "https://exam.elevateegy.com/api/v1/auth/changePassword"
-    static DELETE = "https://exam.elevateegy.com/api/v1/auth/deleteMe"
-    static EDITE_PROFILE = "https://exam.elevateegy.com/api/v1/auth/editProfile"
-    static LOG_OUT = "https://exam.elevateegy.com/api/v1/auth/logout"
-    static USER_INFO = "https://exam.elevateegy.com/api/v1/auth/profileData"
-    static FORGET_PASSWORD = "https://exam.elevateegy.com/api/v1/auth/forgotPassword"
-    static VERIFY_RESET_CODE = "https://exam.elevateegy.com/api/v1/auth/verifyResetCode"
-    static RESET_PASSWORD = "https://exam.elevateegy.com/api/v1/auth/resetPassword"
+    static LOGIN = "v1/auth/signin"
+    static REGISTER = "v1/auth/signup"
+    static CHANGE_PASSWORD = "v1/auth/changePassword"
+    static DELETE = "v1/auth/deleteMe"
+    static EDITE_PROFILE = "v1/auth/editProfile"
+    static LOG_OUT = "v1/auth/logout"
+    static USER_INFO = "v1/auth/profileData"
+    static FORGET_PASSWORD = "v1/auth/forgotPassword"
+    static VERIFY_RESET_CODE = "v1/auth/verifyResetCode"
+    static RESET_PASSWORD = "v1/auth/resetPassword"
 }
