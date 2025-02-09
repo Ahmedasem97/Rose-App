cec6412817dfe3c5e01ac1a14ea054847f946199
Mohamedniem <engdida2020@gmail.com>
2025-02-09 20:55:37 +0200

    edits in routing and create allLayout

--- src/app/core/pages/home/home.component.ts
+++ src/app/core/pages/home/home.component.ts
@@ -15,7 +15,7 @@ import { TrustedComponent } from '../../../shared/components/ui/trusted/trusted.
 import {  BestSeller1Component } from "../../../shared/components/business/pest-seller1/pest-seller1.component";
 import { ReviewComponent } from "../../../shared/components/ui/review/review.component";
 import { FeaturesComponent } from "../../../shared/components/ui/features/features.component";
-import { CateogryComponent } from "../../../shared/components/business/cateogry/cateogry.component";
+
 
 @Component({
   selector: 'app-home',
