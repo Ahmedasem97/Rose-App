f4fe162a7fdb6bdf6b89f973ff8c536107143b70
Mohamedniem <engdida2020@gmail.com>
2025-02-12 19:55:01 +0200

    start product details

--- src/app/shared/components/business/product-card/product-card.component.ts
+++ src/app/shared/components/business/product-card/product-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, input, InputSignal } from '@angular/core';
 import { PopularProduct } from '../../../../core/interfaces/products';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss'
 })
