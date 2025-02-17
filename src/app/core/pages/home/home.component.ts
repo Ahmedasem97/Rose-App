f049f28fd70a3d0e413ba853da7c50a42ee9830f
Ahmed Assem <ahmedasem224@gmail.com>
2025-02-17 22:07:32 +0200

    add after & before by tailwind

--- src/app/core/pages/home/home.component.ts
+++ src/app/core/pages/home/home.component.ts
@@ -16,6 +16,7 @@ import { BestSeller1Component } from '../../../shared/components/business/pest-s
 import { ReviewComponent } from '../../../shared/components/ui/review/review.component';
 import { FeaturesComponent } from '../../../shared/components/ui/features/features.component';
 import { AboutUsComponent } from '../../../shared/components/ui/about-us/about-us.component';
+import { OurTeamComponent } from "../../../shared/components/ui/our-team/our-team.component";
 
 @Component({
   selector: 'app-home',
@@ -30,7 +31,8 @@ import { AboutUsComponent } from '../../../shared/components/ui/about-us/about-u
     BestSeller1Component,
     ReviewComponent,
     FeaturesComponent,
-  ],
+    OurTeamComponent
+],
 
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
