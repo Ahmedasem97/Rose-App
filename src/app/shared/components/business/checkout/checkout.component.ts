e74990476571d5beb148061e720ce2c76644da25
Ahmed Ashraf <a.ashraf.elbendary@gmail.com>
2025-02-25 02:03:10 +0300

    Add Google Map To capture lat & lng

--- src/app/shared/components/business/checkout/checkout.component.ts
+++ src/app/shared/components/business/checkout/checkout.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  viewChild,
+} from '@angular/core';
 import { PaymentSectionTitleComponent } from '../../ui/payment-section-title/payment-section-title.component';
 import {
   FormControl,
@@ -6,6 +12,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { GoogleMapsModule } from '@angular/google-maps';
 import { CustomInputComponent } from '../custom-input/custom-input.component';
 import { PHONE_PATTERN } from '../../../../core/environment/environment';
 
@@ -16,12 +23,34 @@ import { PHONE_PATTERN } from '../../../../core/environment/environment';
     PaymentSectionTitleComponent,
     CustomInputComponent,
     ReactiveFormsModule,
+    GoogleMapsModule,
   ],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss',
 })
-export class CheckoutComponent implements OnInit {
+export class CheckoutComponent implements OnInit, AfterViewInit {
   shippingForm!: FormGroup;
+
+  // Setup the map
+
+  map!: google.maps.Map;
+  center = new google.maps.LatLng(31.214639, 29.945708); // Smouha, Alexandria as default
+  mapOptions: google.maps.MapOptions = {
+    center: this.center,
+    zoom: 15,
+  };
+
+  marker = {
+    position: {
+      lat: 31.214639,
+      lng: 29.945708,
+    },
+    options: {
+      animation: google.maps.Animation,
+    },
+  };
+
+  mapInit() {}
 
   initShippingForm() {
     this.shippingForm = new FormGroup({
@@ -33,10 +62,34 @@ export class CheckoutComponent implements OnInit {
       ]),
       city: new FormControl('', [Validators.required]),
       lat: new FormControl('', [Validators.required]),
-      long: new FormControl('', [Validators.required]),
+      lng: new FormControl('', [Validators.required]),
     });
   }
 
+  get markerOptions() {
+    return this.marker;
+  }
+
+  mapClick(event: google.maps.MapMouseEvent) {
+    if (event.latLng) {
+      const lat = event.latLng.lat();
+      const lng = event.latLng.lng();
+      this.marker.position = { lat, lng };
+      console.log('Latitude:', lat);
+      console.log('Longitude:', lng);
+
+      this.shippingForm.patchValue({
+        lat: lat,
+        lng: lng,
+      });
+
+      console.log(this.shippingForm.value);
+    }
+  }
+
+  ngAfterViewInit() {
+    this.mapInit();
+  }
   ngOnInit(): void {
     this.initShippingForm();
   }
