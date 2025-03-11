5672596b0b85e007ee0ce94eff4e9897f07eaa39
Ahmed Ashraf <a.ashraf.elbendary@gmail.com>
2025-03-12 01:33:41 +0300

    Add Order Service

--- src/app/core/abstract/order.abstract.ts
+++ src/app/core/abstract/order.abstract.ts
@@ -2,11 +2,15 @@ import { Observable } from 'rxjs';
 import {
   CashOrderRes,
   CheckoutSessionRes,
+  ShippingAddress,
   UsersOrdersRes,
 } from '../interfaces/Orders';
 
 export abstract class OrdersAbstract {
   abstract getUserOrders(): Observable<UsersOrdersRes>;
-  abstract createCashOrder(): Observable<CashOrderRes>;
-  abstract checkoutSession(url: string): Observable<CheckoutSessionRes>;
+  abstract createCashOrder(data: ShippingAddress): Observable<CashOrderRes>;
+  abstract checkoutSession(
+    url: string,
+    data: ShippingAddress
+  ): Observable<CheckoutSessionRes>;
 }
